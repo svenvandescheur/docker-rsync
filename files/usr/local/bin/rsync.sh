dd78edd068ae9010a17369be4ede00f8d3240d9c
Sven van de Scheur <svenvandescheur@gmail.com>
2017-01-07 14:19:37 +0100

    Add letsencrypt to rsync dirs

--- files/usr/local/bin/rsync.sh
+++ files/usr/local/bin/rsync.sh
@@ -10,11 +10,18 @@
 
 
 # Create target directories
+ssh -t svenv.nl@recovery.svenv.nl "mkdir -p ~/recovery/etc/letsencrypt/"
 ssh -t svenv.nl@recovery.svenv.nl "mkdir -p ~/recovery/srv/nginx/svenv/media/"
 ssh -t svenv.nl@recovery.svenv.nl "mkdir -p ~/recovery/srv/postgresql/"
 
+
+# Create letsencrypt backup (work around 5 certs / 7 days limitation)
+rsync -a /etc/letsencrypt/ svenv.nl@recovery.svenv.nl:~/recovery/etc/letsencrypt/
+
+
 # Create media backup
 rsync -a /srv/nginx/svenv/media/ svenv.nl@recovery.svenv.nl:~/recovery/srv/nginx/svenv/media/
+
 
 # Create database backup
 rsync -a /srv/postgresql/ svenv.nl@recovery.svenv.nl:~/recovery/srv/postgresql/
